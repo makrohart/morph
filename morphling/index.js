165f9ee76c9d44b60a2c65c8ea517cf03fdcb3ec
jini <haotian.jin@hotmail.com>
2025-09-21 12:51:06 +0800

    Refine code

--- morphling/index.js
+++ morphling/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CustomRenderer from './morphling.js';
-import { App } from './main.js';
+import { App } from './app.js';
 
 // 创建根容器
 const index_html = new View();
